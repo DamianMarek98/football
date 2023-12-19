900df916ede5ebaac8893cb27134a4bf92e19f78
damianm <damian.marek@incata.it>
2023-12-19 23:35:49 +0100

    save game journey on start

--- data/src/main/java/deny/football/data/journey/JourneyGameStarter.java
+++ data/src/main/java/deny/football/data/journey/JourneyGameStarter.java
@@ -1,18 +1,23 @@
 package deny.football.data.journey;
 
+import deny.football.data.infrastructure.journey.JourneyGameRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JourneyGameStarter {
     private final JourneyGameFactory journeyGameFactory;
+    private final JourneyGameRepo journeyGameRepo;
 
     @Autowired
-    public JourneyGameStarter(JourneyGameFactory journeyGameFactory) {
+    public JourneyGameStarter(JourneyGameFactory journeyGameFactory, JourneyGameRepo journeyGameRepo) {
         this.journeyGameFactory = journeyGameFactory;
+        this.journeyGameRepo = journeyGameRepo;
     }
 
     public JourneyGame startGame() {
-        return journeyGameFactory.createGameJourney();
+        JourneyGame gameJourney = journeyGameFactory.createGameJourney();
+        journeyGameRepo.save(gameJourney);
+        return gameJourney;
     }
 }
