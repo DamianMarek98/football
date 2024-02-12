21f1819b1a2665912587e7c9cfb31526bf7b1195
damianm <damian.marek@incata.it>
2024-02-13 00:45:54 +0100

    add not working test for action verification

--- data/src/test/java/deny/football/data/DataApplicationTests.java
+++ data/src/test/java/deny/football/data/DataApplicationTests.java
@@ -3,11 +3,18 @@ package deny.football.data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class DataApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		assertThat(1).isEqualTo(0);
 	}
 
 }
